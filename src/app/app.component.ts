c68030dd758c68b5ef87a5f05458f304c48075f6
christian baidoo <chris_pens@hotmail.co.uk>
2020-06-25 23:52:42 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular app';
+  tester = 'Im starting to like this';
+  students = {
+    name: 'John',
+    age: 24,
+  };
 }
